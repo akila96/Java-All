2e552d3b73ee4b982901d0b3ac0cddd203af106b
akila96 <harshanaakila96@gmail.com>
2022-10-10 10:16:44 +0530

    6th time

---
+++ 6/SecondsandMinit/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(getDurationString(65,45));
+        System.out.println(getDurationString(3945L));
+
+    }
+
+    private static String getDurationString(long minutes , long second ){
+
+
+        if ((minutes<0) || (second < 0) ||(second > 59))
+        {
+             return "invalid number";
+        }
+        long hours= minutes / 60;
+        long remainingMinutes = minutes % 60;
+        return hours +  "h " + remainingMinutes  + "m " + second +  "s" ;
+    }
+
+    private  static String getDurationString(long second){
+      if (second < 0){
+          return  "invalid number";
+      }
+
+      long minutes =second / 60;
+      long remainngSeconds = second % 60 ;
+      String x = getDurationString(minutes,remainngSeconds);
+      return x;
+
+    }
+}
