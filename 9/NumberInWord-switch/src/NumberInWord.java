46242777af2936395460ee0d52f0d0c589969675
akila96 <harshanaakila96@gmail.com>
2022-10-11 08:16:32 +0530

    9th time

---
+++ 9/NumberInWord-switch/src/NumberInWord.java
@@ -0,0 +1,31 @@
+public class NumberInWord {
+
+    public static void printNumberInWord(int number){
+
+      switch (number) {
+
+          case 1:
+              System.out.println("ZERO");
+              break;
+          case 2:
+              System.out.println("ONE");
+              break;
+          case 3:
+              System.out.println("two");
+              break;
+          case 4:
+              System.out.println("THREE");
+              break;
+          case 5:
+              System.out.println("Four");
+              break;
+          default:
+              System.out.println("negative");
+
+
+        }
+
+
+      }
+
+}
